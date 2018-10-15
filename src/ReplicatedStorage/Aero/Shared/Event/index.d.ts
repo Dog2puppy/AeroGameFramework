e0934c59a47f2ee40c7064fa357981251c4d630a
Dog2puppy <colecooper2005@icloud.com>
2018-10-15 19:07:20 -0400

    Event type definition

---
+++ src/ReplicatedStorage/Aero/Shared/Event/index.d.ts
@@ -0,0 +1,12 @@
+interface Connection {
+	Disconnect(): void
+}
+
+interface Event {
+	new ()
+
+	Fire(arguments: any): void
+	Wait(): void
+	DisconnectAll(): void
+	Connect(func: Function): void
+}
